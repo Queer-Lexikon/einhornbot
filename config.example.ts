42920a707f2f1377158fba589f5531d809fff1ab
wea_ondara <wea_ondara@alpenblock.net>
2022-03-05 17:45:41 +0100

    cleanup

---
+++ config.example.ts
@@ -0,0 +1,21 @@
+export default {
+  homeserverUrl: "https://matrix.example.com", //no slash at the end
+  servername: 'example.com',
+  userId: '@user:example.com',
+  accessToken: "<token>",
+
+
+  //command room id
+  commandRoomId: "!channelid:example.com",
+
+  //lock/unlock
+  lockLevel: 50,
+  lockRoomInclude: {
+    '!someroom:example.com': 'Channel 1',
+  },
+
+  //invite to all rooms
+  inviteRoomInclude: {
+    '!someroom:example.com': 'Channel 1',
+  },
+}
