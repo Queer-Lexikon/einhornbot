69a57177dd849b256576bac5979a5f398b584cd2
Aurora <21148213+aurorasmiles@users.noreply.github.com>
2023-10-05 18:39:57 +0200

    Add more explanation to example config

--- config.example.ts
+++ config.example.ts
@@ -1,18 +1,18 @@
 export default {
   homeserverUrl: "https://matrix.example.com", //no slash at the end
-  servername: 'example.com',
-  userId: '@user:example.com',
-  accessToken: "<token>",
+  servername: 'example.com', 
+  userId: '@user:example.com', //the userID the bot should use
+  accessToken: "<token>", //a valid access token for that user
 
 
   //command room id
-  commandRoomId: "!channelid:example.com",
+  commandRoomId: "!channelid:example.com", //room for commands, must be unencrypted
 
   //lock/unlock
-  lockLevel: 50,
+  lockLevel: 50, // power level to set send messages and add reactions for the given channels
   lockRoomInclude: {
     '!someroom:example.com': 'Channel 1',
-  },
+  }, 
 
   //invite to all rooms
   inviteRoomInclude: {
